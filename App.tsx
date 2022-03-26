77862aac1b00a5ba19d262616aa4583482fd2532
Karol Harumi <karol.harumi@pebmed.com.br>
2022-03-26 11:59:27 -0400

    Feat: Configuring async storage

--- App.tsx
+++ App.tsx
@@ -4,15 +4,44 @@ import Styles from './components/Styles';
 import PersonalInfo from './components/PersonalInfo';
 import Chat from './components/Chat';
 import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [username, setUsername] = useState("");
   const [image, setImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  const onPersonalInfoClosed = (name: string, image: string) => {
-    setUsername(name);
+  const storageUsernameKey = "chatapp-username";
+  const storageImageKey = "chatapp-image";
+
+  const fetchPersonalData = async () => {
+    let fetchedUsername = await AsyncStorage.getItem(storageUsernameKey);
+    let username = fetchedUsername == null ? "" : fetchedUsername;
+
+    let fetchedImage = await AsyncStorage.getItem(storageImageKey);
+    let image = fetchedImage == null ? "" : fetchedImage;
+
+    setUsername(username);
     setImage(image);
+  }
+
+  const onPersonalInfoClosed = async (name: string, image: string) => {
+    setUsername(name);
+    await AsyncStorage.setItem(storageUsernameKey, name);
+    setImage(image);
+    await AsyncStorage.setItem(storageImageKey, image);
   };
+
+  if (isLoading) {
+    return (
+      <AppLoading
+        startAsync={fetchPersonalData}
+        onFinish={() => setIsLoading(false)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   let activeComponent = username != "" ? (
     <Chat username={ username } image={ image } /> 
